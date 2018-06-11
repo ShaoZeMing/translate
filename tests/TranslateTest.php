2bc7eaacc6b0519995579ca7c6127e041411a751
shaozeming <szm19920426@gmail.com>
2018-06-11 22:01:55 +0800

    添加单元测试

---
+++ tests/TranslateTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *  TestSms.php
+ *
+ * @author szm19920426@gmail.com
+ * $Id: TestSms.php 2017-08-17 上午10:08 $
+ */
+
+namespace ShaoZeMing\Translate\Test;
+require_once dirname(__FILE__) . '/../src/getui/IGt.Push.php';
+
+use PHPUnit\Framework\TestCase;
+use ShaoZeMing\Translate\TranslateService;
+
+
+class TranslateTest extends TestCase
+{
+    protected $instance;
+
+    public function setUp()
+    {
+
+        $file =  dirname(__DIR__) .'/config/translate.php';
+        $config = include($file);
+        $this->instance = new TranslateService($config);
+    }
+
+
+    public function testPushManager()
+    {
+        $this->assertInstanceOf(TranslateService::class, $this->instance);
+    }
+
+
+    public function testPush()
+    {
+        echo PHP_EOL."发送push 中....".PHP_EOL;
+        try {
+            $result =  $this->instance->translate('你知道我对你不仅仅是喜欢');
+            print_r($result);
+            return $result;
+        } catch (\Exception $e) {
+            $err = "Error : 错误：" . $e->getMessage();
+            echo $err.PHP_EOL;
+
+        }
+    }
+}
